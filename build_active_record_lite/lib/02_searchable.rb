c353d2673464a5ddf8219a11adfc3bb68f4767d4
Ray Munoz <Raymond_Munoz@alumni.brown.edu>
2019-09-09 16:42:14 -0400

    Finish 02_searchable

--- build_active_record_lite/lib/02_searchable.rb
+++ build_active_record_lite/lib/02_searchable.rb
@@ -3,10 +3,20 @@ require_relative '01_sql_object'
 
 module Searchable
   def where(params)
-
+    param_mapping = params.keys.map { |key| "#{key} = ?" }
+    results = DBConnection.execute(<<-SQL, *params.values)
+      SELECT
+        *
+      FROM
+        #{self.table_name}
+      WHERE
+        #{param_mapping.join(' AND ')}
+    SQL
+    self.parse_all(results)
   end
 end
 
 class SQLObject
   # Mixin Searchable here...
+  extend Searchable
 end
